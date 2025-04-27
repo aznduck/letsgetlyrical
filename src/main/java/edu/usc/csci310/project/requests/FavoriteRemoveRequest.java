70074f35383155b0431b7dff76c77a1533aa2d20
Felix <felixzc8@gmail.com>
2025-04-27 15:18:51 -0700

    refactor 104

--- src/main/java/edu/usc/csci310/project/requests/FavoriteRemoveRequest.java
+++ src/main/java/edu/usc/csci310/project/requests/FavoriteRemoveRequest.java
@@ -2,16 +2,7 @@ package edu.usc.csci310.project.requests;
 
 public class FavoriteRemoveRequest {
     private String username;
-    private String password;
     private int songId;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public String getUsername() {
         return username;
